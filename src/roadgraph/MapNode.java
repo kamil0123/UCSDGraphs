84fa803cf26cddf22effb0c02d6110fef7272a25
Kamil Kindziuk <kamil.kindziuk@gmail.com>
2017-07-25 11:25:10 +0200

    Implement MapNode class

--- src/roadgraph/MapNode.java
+++ src/roadgraph/MapNode.java
@@ -1,5 +1,16 @@
 package roadgraph;
 
-public class MapNode {
+import geography.GeographicPoint;
 
+public class MapNode {
+	
+	public final GeographicPoint vertex;
+	public final String streetName;
+	public final String streetType;
+	
+	public MapNode(GeographicPoint vertex, String name, String type) {
+		this.vertex = vertex;
+		this.streetName = name;
+		this.streetType = type;
+	}
 }
